c99faf7261c17b4b1165d065d98971b067e607f8
Mike G <mikegii.prog@gmail.com>
2025-08-17 12:45:50 +0300

    34 - Added updates section to the main page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,6 +5,7 @@ import { Hero } from '../components/home/Hero';
 import { Features } from '../components/home/Features';
 import { GameInfo } from '../components/home/GameInfo';
 import { CallToAction } from '../components/home/CallToAction';
+import { Updates } from '../components/home/Updates';
 
 const HomePage: React.FC = () => {
     return (
@@ -14,6 +15,7 @@ const HomePage: React.FC = () => {
             <Features />
             <GameInfo />
             <CallToAction />
+            <Updates />
         </div>
     );
 };
