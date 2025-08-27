aa2c342a25b48c04fe254b6c3c8cf3acdf6bd4b8
Mike G <mikegii.prog@gmail.com>
2025-08-28 01:01:30 +0300

    147 - Added crime activity feature to department leaderboard

--- src/components/admin/AdminTools.tsx
+++ src/components/admin/AdminTools.tsx
@@ -4,6 +4,7 @@ import { useToast } from '../../contexts/ToastContext';
 import { CheatDetector } from './tools/CheatDetector';
 import { LevelRecalculator } from './tools/LevelRecalculator';
 import { WorkManager } from './tools/WorkManager';
+import { CrimeInitializer } from './tools/CrimeInitializer';
 import '../../styles/components/admin/AdminTools.css';
 
 export const AdminTools: React.FC = () => {
@@ -28,6 +29,12 @@ export const AdminTools: React.FC = () => {
                 <div className="tool-category">
                     <h3 className="category-title">Mängijate Haldus</h3>
                     <LevelRecalculator />
+                </div>
+
+                {/* Crime System */}
+                <div className="tool-category">
+                    <h3 className="category-title">Kuritegevuse Süsteem</h3>
+                    <CrimeInitializer />
                 </div>
 
                 {/* Security Tools */}
